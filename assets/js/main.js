6c1e0af2c8fc5b22d2abcb9b572c3456c8c7a0ea
Mohammed Abdul Hafeez Khan <82383549+hafeezkhan909@users.noreply.github.com>
2025-08-28 12:40:43 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,21 @@
-// Lightbox logic
-document.querySelectorAll('.pub-media img').forEach(img => {
-  img.addEventListener('click', function() {
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImg = document.getElementById('lightbox-img');
-    lightbox.style.display = 'flex';
-    lightboxImg.src = this.src;
+document.addEventListener("DOMContentLoaded", function () {
+  // Lightbox logic
+  document.querySelectorAll('.pub-media img').forEach(img => {
+    img.addEventListener('click', function () {
+      const lightbox = document.getElementById('lightbox');
+      const lightboxImg = document.getElementById('lightbox-img');
+      lightbox.style.display = 'flex';
+      lightboxImg.src = this.src;
+    });
   });
-});
 
-document.querySelector('#lightbox .close').addEventListener('click', function() {
-  document.getElementById('lightbox').style.display = 'none';
-});
+  document.querySelector('#lightbox .close').addEventListener('click', function () {
+    document.getElementById('lightbox').style.display = 'none';
+  });
 
-document.getElementById('lightbox').addEventListener('click', function(e) {
-  if (e.target === this) {
-    this.style.display = 'none';
-  }
+  document.getElementById('lightbox').addEventListener('click', function (e) {
+    if (e.target === this) {
+      this.style.display = 'none';
+    }
+  });
 });
